2645c1af149bbb78c75d0fac5f2e713688849204
Ankit-Ransh <ankitanand2125@gmail.com>
2024-01-25 22:18:20 +0530

    Limited Projects Deployment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mini-Project
 This repo contains mini projects designed using HTML, CSS, Javascript.
+
--- To Do List/index.js
+++
@@ -1,6 +0,0 @@
-const addTodo = () => {
-    console.log(document.querySelector(".todo"));
-}
-
-let input = document.querySelector(".todos");
-input.addEventListener("submit", addTodo);
